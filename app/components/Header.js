94bc990ba0f2ddb871d227175bac602a27f544bc
Tom Schmidt <sittingpilgrim@gmail.com>
2020-07-24 13:47:27 -0700

    Simple change title

--- app/components/Header.js
+++ app/components/Header.js
@@ -14,7 +14,7 @@ function Header(props) {
       <header className="header-bar bg-primary mb-3">
          <div className="container d-flex flex-column flex-md-row align-items-center p-3">
             <h4 className="my-0 mr-md-auto font-weight-normal">
-               <Link to="/" className="text-white"> ComplexApp! </Link>
+               <Link to="/" className="text-white"> ComplexApp </Link>
             </h4>
             {!props.staticEmpty ? headerContent : ''}
          </div>
